dd811304da8d57e53e21ab49882902f6220e35bc
AnhChuongChovy <chuonggo51@gmail.com>
2025-01-13 20:15:32 +0700

    Tạo project

---
+++ BaiTap1/BaiTap1/DATA/AppDBContext.cs
@@ -0,0 +1,29 @@
+﻿using BaiTap1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiTap1.DATA
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Cấu hình quan hệ 1 - N giữa Course và Enrollment
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.CourseID);
+
+            // Cấu hình quan hệ 1 - N giữa Student và Enrollment
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.StudentID);
+        }
+    }
+}
---
+++ BaiTap1/BaiTap1/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap1.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Range(1, 10)]
+        public int Credits { get; set; }
+
+        // Quan hệ 1 - N với Enrollment
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
