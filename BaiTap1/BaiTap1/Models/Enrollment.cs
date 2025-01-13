9d7da40b6db7a2c63e99acb402cd23404d9f152d
AnhChuongChovy <chuonggo51@gmail.com>
2025-01-13 20:15:32 +0700

    Tạo project

---
+++ BaiTap1/BaiTap1/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap1.Models
+{
+    public class Enrollment
+    {
+        [Key]
+        public int EnrollmentID { get; set; }
+
+        // Khóa ngoại liên kết với Course
+        [ForeignKey("Course")]
+        public int CourseID { get; set; }
+
+        // Khóa ngoại liên kết với Student
+        [ForeignKey("Student")]
+        public int StudentID { get; set; }
+
+        public string Grade { get; set; }
+
+        // Thuộc tính điều hướng (Navigation Properties)
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
